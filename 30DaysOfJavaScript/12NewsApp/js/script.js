bbe0178ab4fdf3c54d956acd58f8694944361a2d
simplykamar <kamaralam366@gmail.com>
2022-09-21 15:09:29 +0530

    day10

--- 30DaysOfJavaScript/12NewsApp/js/script.js
+++ 30DaysOfJavaScript/12NewsApp/js/script.js
@@ -3,28 +3,27 @@ async function get(){
 	let context;
 	let img = "img/not-found.jpg";
 	let desc="";
-	let data = await fetch('https://newsdata.io/api/1/news?country=in&apikey=pub_11409423982f8c2dc6b488cd3ecfc65df6e92');
+	let data = await fetch('https://gnews.io/api/v4/top-headlines?token=f1540dab510ac45fd4f5f0e6d83e0074&country=in');
 	data = await data.json();
 	console.log(data);
-
-	data = data.results;
+	data = data.articles;
 	console.log(data);
 	data.forEach(function(obj){
-		if(obj.image_url)
-			img = obj.image_url;
+		if(obj.image)
+			img = obj.image;
 		if (obj.description)
 			desc = obj.description;
 		context = `
 				<li class="list-group-item list-group-item-action list-group-item-dark m-1">
 					<div class="row">
-						<div class="col-lg-4 col-sm-4 col-md-4 col-4">
+						<div class="col-lg-4 col-sm-12 col-md-4 col-12">
 							<img src="${img}" class="img-fluid" alt="Image not found">		
 						</div>
-						<div class="col-lg-8 col-sm-8 col-md-8 col-8">
-							<a href=${obj.link}><h4>${obj.title}</h4></a>
+						<div class="col-lg-8 col-sm-12 col-md-8 col-12">
+							<a href=${obj.url}><h4>${obj.title}</h4></a>
 							<p>${desc}</p>
 								<div class="text-right">
-									<span class="badge badge-dark badge-pill">${obj.pubDate}</span>
+									<span class="badge badge-dark badge-pill">${obj.publishedAt}</span>
 								</div>
 						</div>
 					</div>
